c09e076524829e33dc7e1c89b9c00f51f5dbd31f
Chris Zempel <zempel@efeqdev.com>
2015-12-06 22:31:03 -0600

    little animation for voting thing

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -6,10 +6,14 @@ class VotesController < ApplicationController
     @vote = current_user.votes.new vote_params
     @vote.photo = @photo
 
-    if @vote.save
-      redirect_to @photo, notice: "Your vote has been tallied"
-    else
-      redirect_to @photo, notice: "Problems Receiving Your Vote"
+    respond_to do |format|
+      if @vote.save
+        format.js
+        format.html { redirect_to @photo, notice: "Your vote has been tallied"}
+      else
+        format.js { redirect_to @photo, notice: "Problems Receiving Your Vote" }
+        format.html {redirect_to @photo, notice: "Problems Receiving Your Vote" }
+      end
     end
   end
 
